4d93510ef1307b28a6b2ad4027b9679cf648ea55
袁杰 <748468398@qq.com>
2024-06-10 22:06:32 +0800

    fix: update:

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+  });
+}
+
 </script>
 
 <template>
